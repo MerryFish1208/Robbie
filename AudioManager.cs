fab99a0595e8f6d38eacd3138db528367dc0b4c6
MerryFish1208 <61783233+MerryFish1208@users.noreply.github.com>
2020-03-10 21:06:41 +0800

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager current;
+
+    [Header("环境声音")]
+    public AudioClip ambientClip;
+    public AudioClip musicClip;
+    [Header("FX音效")]
+    public AudioClip deathFXClip;
+
+    [Header("player声音")]
+    public AudioClip[] walkStepClips;
+    public AudioClip[] crouchStepClips;
+    public AudioClip jumpClip;
+    public AudioClip jumpVoiceClip;
+    public AudioClip deathClip;
+    public AudioClip deathVoiceClip;
+
+
+    AudioSource ambientSource;
+    AudioSource musicSource;
+    AudioSource fxSoruce;
+    AudioSource playerSource;
+    AudioSource voiceSource;
+
+
+
+    private void Awake()
+    {
+        current = this;
+        DontDestroyOnLoad(gameObject);//场景切换不销毁该对象
+        ambientSource = gameObject.AddComponent<AudioSource>();//为ambientSource添加AudioSource组件
+        musicSource = gameObject.AddComponent<AudioSource>();
+        fxSoruce = gameObject.AddComponent<AudioSource>();
+        playerSource = gameObject.AddComponent<AudioSource>();
+        voiceSource = gameObject.AddComponent<AudioSource>();
+        StartLevelAudio();
+    }
+
+    void StartLevelAudio()
+    {
+        current.ambientSource.clip = current.ambientClip;
+        current.ambientSource.loop = true;
+        current.ambientSource.Play();
+
+        current.musicSource.clip = current.musicClip;
+        current.musicSource.loop = true;
+        current.musicSource.Play();
+    }
+
+    public static void PlayFootstepAudio()
+    {
+        int index = Random.Range(0, current.walkStepClips.Length);
+        current.playerSource.clip = current.walkStepClips[index];
+        current.playerSource.Play();
+
+    }
+    public static void PlayCrouchFootstepAudio()
+    {
+        int index = Random.Range(0, current.crouchStepClips.Length);
+        current.playerSource.clip = current.crouchStepClips[index];
+        current.playerSource.Play();
+
+    }
+    public static void PlayJumpAudio()
+    {
+        current.playerSource.clip = current.jumpClip;
+        current.playerSource.Play();
+
+        current.voiceSource.clip = current.jumpVoiceClip;
+        current.voiceSource.Play();
+    }
+    public static void PlayDeathAudio()
+    {
+        current.playerSource.clip = current.deathClip;
+        current.playerSource.Play();
+
+        current.voiceSource.clip = current.deathVoiceClip;
+        current.voiceSource.Play();
+
+        current.fxSoruce.clip = current.deathFXClip;
+        current.fxSoruce.Play();
+    }
+
+
+}
